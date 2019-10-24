00a1503770af91da5ef4b4b5eecce38bb2897f77
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2019-10-24 11:49:51 +0300

    init

---
+++ gosandbox.go
@@ -0,0 +1,59 @@
+package gosandbox
+
+import (
+	"github.com/tidwall/gjson"
+	"github.com/tidwall/sjson"
+)
+
+type UserFunc func(data data.Data, cancel <-chan struct{}) (data.Data, error)
+
+type Data struct {
+	json string
+}
+
+func NewData(json string) Data {
+	return Data{
+		json: json,
+	}
+}
+
+func (d *Data) Unset(path string) error {
+	json, err := sjson.Delete(d.json, path)
+	if err == nil {
+		d.json = json
+	}
+
+	return err
+}
+
+func (d *Data) Set(path string, v string) error {
+	json, err := sjson.Set(d.json, path, v)
+	if err == nil {
+		d.json = json
+	}
+
+	return err
+}
+
+func (d *Data) SetInt64(path string, v int64) error {
+	json, err := sjson.Set(d.json, path, v)
+	if err == nil {
+		d.json = json
+	}
+
+	return err
+}
+
+func (d *Data) GetInt64(path string) int64 {
+	return gjson.Get(d.json, path).Int()
+}
+
+func (d *Data) Get(path string) string {
+	return gjson.Get(d.json, path).String()
+}
+
+func (d *Data) String() string {
+	return d.json
+}
+
+
